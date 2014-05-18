469c3c077d79f6ed002fe9d3ceaaad10a57e7bc0
unknown <adrianojlt@gmail.com>
2014-05-18 23:31:36 +0100

    struts-config.xml ... main sql tables

---
+++ RNU/RNU_DataModel.sql
@@ -0,0 +1,18 @@
+select * from IDU_IDENT_UT;
+select * from IDU_INSCR;
+
+select * from IDU_FAMILIA;
+select * from IDU_HIST_BENEF;
+select * from IDU_BENEF_UT;
+select * from IDU_CONTACTOS_UT;
+
+select * from SYS_USERS;
+select * from SYS_USERS_PERFIS;
+select * from SYS_PERFIS;
+select * from SYS_ENTIDADES;
+select * from SYS_COD_GENERICOS;
+select * from PRO_PROFISSID_PESSOAL;
+
+
+
+
